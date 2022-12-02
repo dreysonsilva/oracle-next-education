9d9fe8e322a7212361b9ef66c679129abde6acc7
Dreyson Santiago <dreysoninformar@gmail.com>
2022-12-01 22:32:20 -0300

    alterações no script estudos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-document.write("Hello");
+//PROGRAMA IMC 
+var nome = prompt("Informe seu nome");
+var alturaInformada = prompt(nome+ " informe sua altura?");
+var pesoInformado = prompt(nome+ " qual seu peso?");
+var imc=pesoInformado / (alturaInformada*alturaInformada);
+
+document.write("sua altura é: "+alturaInformada+"<br>");
+document.write("Seu peso: "+pesoInformado+"<br>");
+document.write("Olá "+nome+" imc)<br><br>");
+
+
+
+//FUNÇÕES ou function
+function pulaLinha(){
+    document.write("</br></br></br></br>");
+}
+function soma(numeroUm,numeroDois){
+    document.write("A soma do numero 1 + numero Dois = "+(numeroUm+numeroDois));
+}
+
+soma(10,20);
+
+
+var tanque = 40;
+
+var caminhoComGasolina = 480;
+var consumoDeGasolina = caminhoComGasolina/tanque;
+
+var caminhoComAlcool = 300;
+var consumoDeAlcool = caminhoComAlcool/tanque;
+
+document.write("O consumo de Gasolina é " + consumoDeGasolina + " km/L"+pulaLinha());
+document.write("O consumo de Álcool é " + consumoDeAlcool + " km/L");
